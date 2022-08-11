8795dbe907f291a990c5fc0f9ebbb552728230f7
Rajani462 <53518303+Rajani462@users.noreply.github.com>
2022-08-11 15:53:27 +0200

    Spatial mean daily for 2001 - 20 updated

---
+++ code/10_plot_spatial_mean_daily_rf.R
@@ -0,0 +1,54 @@
+source('./source/libs.R')
+source('./source/themes.R')
+source('./source/palettes.R')
+
+library(viridis)
+library(rnaturalearth)
+library(rgdal)
+
+##########
+
+mean_2001_20 <- readRDS("./data/imerg_spat_mean_25ns_2001_20.rds")
+ind_rama_imrg <- readRDS("./data/ind_rama_imrg.rds")
+atln_pirata_imrg <- readRDS("./data/atln_pirata_imrg.rds")
+pacf_tao_imrg <- readRDS("./data/pacf_tao_imrg.rds")
+
+###############
+
+ind <- ind_rama_imrg[, .(buoys_rf = mean(ind_rama, na.rm = TRUE)), by = .(lat, lon, sname)]
+atln <- atln_pirata_imrg[, .(buoys_rf = mean(atln_pirata, na.rm = TRUE)), by = .(lat, lon, sname)]
+pacf <- pacf_tao_imrg[, .(buoys_rf = mean(pacf_tao, na.rm = TRUE)), by = .(lat, lon, sname)]
+ind_atln_pacf <- rbind(ind, atln, pacf)
+#rama_atln_pacf <- readRDS("./data/rama_atln_pacf_latlon.rds")
+
+#############
+
+name_shp <- readOGR("C:/Users/rkpra/OneDrive/Documents/R_projects/GPM_review/data/ne_110m_admin_0_countries",
+layer="ne_110m_admin_0_countries")
+
+ggplot(mean_2001_20) +
+geom_tile(aes(lon, lat, fill = mean_rf)) +
+#coord_fixed(ratio = 1) +
+scale_fill_viridis(direction = -1) +
+coord_sf(ylim = c(-25, 25), expand = c(0, 0)) +
+labs(x = "", y = "", fill = "IMERG (mm/day)") +
+geom_polygon(data = name_shp,
+aes(x = long, y = lat, group = group), fill="white", color="black") +
+#scale_color_gradient(low = "gray", high = "blue") +
+#geom_point(data = rama_atln_pacf, aes(lon, lat), colour = "red", size = 1) +
+geom_point(data = ind_atln_pacf, aes(lon, lat, color = buoys_rf), shape = 19, size = 1.5) +
+scale_color_gradientn(colours = c("grey","orange", "red", "black"), limits = c(0, 14)) +
+#scale_color_gradient(low = "gray", high = "red") +
+# guides(limits = c(1,4), fill = guide_colorbar(reverse = TRUE)) +
+# scale_fill_gradient(limits = c(1,4), breaks=c(1,2,3,4))
+labs(color ="Buoys (mm/day)") +
+theme_generic +
+theme(legend.position = "bottom", legend.key.width = unit(1.1, "cm"),
+legend.key.height = unit(0.5, 'cm'))
+
+ggsave("results/paper_fig/spatil_mean_dist.png",width = 8.2, height = 3.3, 
+       units = "in", dpi = 600)
+
+
+###################################################################################
+
