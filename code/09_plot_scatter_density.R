dc33138dbe498315f4aac5af4be66d71ffbe4ac2
Rajani462 <53518303+Rajani462@users.noreply.github.com>
2022-08-24 23:53:54 +0200

    created a separate plot for only hit days and IMERG-F.

--- code/09_plot_scatter_density.R
+++ code/09_plot_scatter_density.R
@@ -39,11 +39,13 @@ ind_alt_pacf_meanplot <- melt(ind_atln_pacf_mean, c("date", 'ocn', 'buyos'), val
 
 stat_met <- ind_alt_pacf_meanplot[, .(N = .N, COR = round(cor(imrg_rf, buyos), 2), 
                               RMSE = round(sqrt(sum((imrg_rf - buyos)^2)/.N), 2), 
-                              Bias =  round(sum(imrg_rf - buyos)/.N, 2)), by = .(variable, ocn)]
+                              MAE = round(sum(abs(imrg_rf - buyos))/.N, 2)),
+                              #Bias =  round(sum(imrg_rf - buyos)/.N, 2)), 
+                              by = .(variable, ocn)]
 
 stat_met2 <- as_tibble(stat_met) %>% group_by(variable) %>%
   group_by(ocn) %>%
-  mutate(lab = paste("N = ", N, "\nCOR = ", COR, "\nRMSE = ", RMSE, "\nBias = ", Bias))
+  mutate(lab = paste("N = ", N, "\nCOR = ", COR, "\nRMSE = ", RMSE, "\nMAE = ", MAE))
 
 
 # plots -------------------------------------------------------------------
@@ -78,13 +80,32 @@ ggplot(ind_alt_pacf_meanplot[buyos >= 0.1 & imrg_rf >= 0.1], aes(x = buyos, y = 
 ggsave("results/paper_fig/scat_dens_hitdays.png",
        width = 7.2, height = 5.3, units = "in", dpi = 600)
 
+### only for imrg_final run
+
+ggplot(ind_alt_pacf_meanplot[variable == "IMERG-F" & buyos >= 0.1 & imrg_rf >= 0.1], aes(x = buyos, y = imrg_rf)) +
+  geom_bin2d(bins = 50) + 
+  labs(x = "Buyos (mm/day)", y = "IMERG (mm/day)") +  
+  geom_abline(intercept = 0, slope = 1, col = "black") + 
+  #scale_fill_continuous(type = "viridis") +  
+  scale_fill_distiller(palette = "Spectral") + 
+  facet_grid(~ocn) + 
+  scale_x_log10()+scale_y_log10()+
+  coord_cartesian(xlim=c(0.1, 220),ylim=c(0.1,220)) + 
+  geom_label(data = stat_met2,
+             aes(x = 95, y = 0.3, label = lab), label.padding = unit(0.1, "lines"), 
+             label.size=0, size = 2.5) + 
+  theme_very_small
+
+ggsave("results/paper_fig/scat_dens_hitdays_imrg_f.png",
+       width = 9.9, height = 3.0, units = "in", dpi = 600)
+
 ### all_days
 
 ind_alt_pacf_meanplot[buyos < 0]
 
 ggplot(ind_alt_pacf_meanplot, aes(x = buyos, y = imrg_rf)) +
   geom_bin2d(bins = 100) + 
-  labs(x = "Buyos (mm/day)", y = "IMERG (mm/day)") + 
+  labs(x = "Buoys (mm/day)", y = "IMERG (mm/day)") + 
   #coord_equal(ratio = 1) + 
   geom_abline(intercept = 0, slope = 1, col = "black") + 
   #scale_fill_continuous(type = "viridis", breaks = c(0, 20, 40, 60, 80 ,100)) +  
@@ -103,6 +124,31 @@ ggplot(ind_alt_pacf_meanplot, aes(x = buyos, y = imrg_rf)) +
 
 ggsave("results/paper_fig/scat_dens_alldays.png",
        width = 7.2, height = 5.3, units = "in", dpi = 600)
+
+
+### only for IMERG-final run
+
+ggplot(ind_alt_pacf_meanplot[variable == "IMERG-F"], aes(x = buyos, y = imrg_rf)) +
+  geom_bin2d(bins = 100) + 
+  labs(x = "Buoys (mm/day)", y = "IMERG (mm/day)") + 
+  #coord_equal(ratio = 1) + 
+  geom_abline(intercept = 0, slope = 1, col = "black") + 
+  #scale_fill_continuous(type = "viridis", breaks = c(0, 20, 40, 60, 80 ,100)) +  
+  #scale_fill_distiller(type = "seq", palette = "Reds") + 
+  scale_fill_distiller(palette = "Spectral", breaks = c(0, 20, 40, 60, 80, 100)) + 
+   facet_wrap(~ocn) + 
+  scale_x_log10() + scale_y_log10() +
+  coord_cartesian(xlim=c(0.1, 220), ylim=c(0.1,220)) + 
+  geom_label(data = stat_met2,
+             aes(x = 115, y = 0.3, label = lab), label.padding = unit(0.1, "lines"), 
+             label.size=0, size = 2.0) + 
+  theme_small
+
+
+ggsave("results/paper_fig/scat_dens_alldays_finalrun.png",
+       width = 7.2, height = 5.3, units = "in", dpi = 600)
+
+
 
 
 library(ggrepel)
@@ -175,7 +221,7 @@ ind_atln_pacf_mean2 <- ind_atln_pacf_mean[, .(date = YearMonth(date), ocn, imrg_
                                               imrg_f, buyos)] 
 
 monthly <- ind_atln_pacf_mean2[, lapply(.SD, sum, na.rm=TRUE), by = .(date, ocn), 
-                              .SDcols=c("imrg_e", "imrg_l", "imrg_f", "buyos")]
+                              .SDcols=c("IMERG_E", "imrg_l", "imrg_f", "buyos")]
 
 monthly_plot <- melt(monthly, c("date", "ocn"))
 
