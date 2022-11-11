060214b86d4853b0be9a2dde7639e6aee65ae264
Rajani462 <53518303+Rajani462@users.noreply.github.com>
2022-11-11 11:56:02 +0100

    added correlation plot as well

--- code/08_plot_boxplot.R
+++ code/08_plot_boxplot.R
@@ -3,44 +3,37 @@ source('./source/themes.R')
 source('./source/palettes.R')
 
 
-# Box_plot ----------------------------------------------------------------
+# import the datsets ----------------------------------------------------------------
 
 ind <- readRDS("./data/metrics_ind.rds")
 atln <- readRDS("./data/metrics_atln.rds")
-pacf <- readRDS("./data/metrics_pacf.rds")
+east_pacf <- readRDS("./data/metrics_east_pacf.rds")
+west_pacf <- readRDS("./data/metrics_west_pacf.rds")
 
 
-# pre-process before plot
-ind <- ind[, .(sname, imrg_run, bias, rmse, mae, POD, FAR, CSI, ocn)]
-atln <- atln[, .(sname, imrg_run, bias, rmse, mae, POD, FAR, CSI, ocn)]
-pacf <- pacf[, .(sname, imrg_run, bias, rmse, mae, POD, FAR, CSI, ocn)]
+# pre-process datsets ----------------------------------------------------------------
+
+ind <- ind[, .(sname, imrg_run, bias, rmse, mae, POD, cor, FAR, CSI, ocn)]
+atln <- atln[, .(sname, imrg_run, bias, rmse, mae, cor, POD, FAR, CSI, ocn)]
+east_pacf <- east_pacf[, .(sname, imrg_run, bias, rmse, mae, cor, POD, FAR, CSI, ocn)]
+west_pacf <- west_pacf[, .(sname, imrg_run, bias, rmse, mae, cor, POD, FAR, CSI, ocn)]
 
 
-pacf <- pacf %>% 
-  mutate(sname2 = sname) %>% 
-  tidyr::separate(col = sname, into = c("lat","lon"), sep = "_")
+met_all <- rbind(ind, atln, east_pacf, west_pacf)
 
-pacf$ocn <- with(pacf, ifelse(lon < 0, "east_pacf", "west_pacf"))
-pacf[, `:=`(sname = sname2, lat = NULL, lon = NULL, sname2 = NULL)]
+# plot ----------------------------------------------------------------
 
-
-met_all <- rbind(ind, atln, pacf)
-
-# ind_alt_pacf_stat <- rbind(ind_stat, atln_stat, pacf_stat2)
-# ind_alt_pacf_stat[, i.tot_mon := NULL]
-
-# plot
 colset_4 <-  c("#D35C37", "#BF9A77", "#D6C6B9", "#97B8C2")
 mycol_continent5 <- c( "#00B0F6", "#A3A500", "#00BF7D", "#e07b39", 
                        "#80391e") 
 
 met_all_plot <- melt(met_all, c("sname", "imrg_run", "ocn"))
 
-levels(met_all_plot$variable) <- c("BIAS", "RMSE", "MAE",  "POD",  "FAR", "CSI")
+levels(met_all_plot$variable) <- c("BIAS", "RMSE", "MAE", "COR", "POD",  "FAR", "CSI")
 levels(met_all_plot$imrg_run) <- c("IMERG-E", "IMERG-L", "IMERG-F")
 levels(met_all_plot$ocn) <- c("Indian", "Atlantic", "East Pacific", "West Pacific")
 
-ggplot(met_all_plot, aes(ocn, value, fill = imrg_run)) + 
+ggplot(met_all_plot[variable != "COR"], aes(ocn, value, fill = imrg_run)) + 
   geom_boxplot() + 
   facet_wrap(~variable, scales = "free_y", ncol = 3) + 
   scale_fill_manual(values = c("808000",  "#D35C37", "#6590bb")) + 
@@ -55,21 +48,30 @@ ggplot(met_all_plot, aes(ocn, value, fill = imrg_run)) +
         axis.text.x = element_text(angle = 30, hjust = 0.8, vjust = 0.9))
 #strip.text.x = element_text(size = 12))
 
+
 ggsave("results/paper_fig/boxplot_metrices.png",
+       width = 7.2, height = 5.5, units = "in", dpi = 600)
+
+
+### with correlation
+
+ggplot(met_all_plot, aes(ocn, value, fill = imrg_run)) + 
+  geom_boxplot() + 
+  facet_wrap(~variable, scales = "free_y", ncol = 3) + 
+  scale_fill_manual(values = c("808000",  "#D35C37", "#6590bb")) + 
+  #scale_fill_manual(values = mycol_continent5[c(4, 3, 2, 1)]) + 
+  #scale_fill_manual(values = c("#b64925", "808000", "#6590bb")) + 
+  labs(x = "", y = "") + 
+  theme_small + # for presentation slides
+  theme(legend.position = "bottom",
+        legend.title = element_blank()) +
+  theme(strip.background = element_rect(fill = "white"),
+        strip.text = element_text(colour = 'Black'),
+        axis.text.x = element_text(angle = 30, hjust = 0.8, vjust = 0.9))
+
+
+ggsave("results/boxplot_metrices.png",
        width = 7.2, height = 5.5, units = "in", dpi = 600)
 
 #################################################################
 
-### mean 
-
-ind_mean <- met_all[imrg_run == "imrg_f" & ocn == "ind"]
-summary(ind_mean)
-
-atln_mean <- met_all[imrg_run == "imrg_f" & ocn == "atln"]
-summary(atln_mean)
-
-east_pacf_mean <- met_all[imrg_run == "imrg_f" & ocn == "east_pacf"]
-summary(east_pacf_mean)
-
-west_pacf_mean <- met_all[imrg_run == "imrg_f" & ocn == "west_pacf"]
-summary(west_pacf_mean)
