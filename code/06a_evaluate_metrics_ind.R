539ac492b6f8c309b4b2beea6c5d8c7f2fbd52fe
Rajani462 <53518303+Rajani462@users.noreply.github.com>
2023-03-06 21:22:38 +0100

    small changes in relative metrices

--- code/06a_evaluate_metrics_ind.R
+++ code/06a_evaluate_metrics_ind.R
@@ -22,10 +22,12 @@ ind_rama_imrg_long <- melt(ind_rama_imrg, c("lat", "lon", "date", "sname", "ind_
 volmet_ind <- ind_rama_imrg_long[, .(ref_mean = mean(ind_rama, na.rm = TRUE), 
                                      bias = sum(imrg_rf - ind_rama)/.N, 
                                      pbias = ((sum(imrg_rf - ind_rama))/sum(ind_rama))*100,
-                                     rbias = ((sum(imrg_rf - ind_rama))/sum(ind_rama)),
+                                     rbias = ((sum(imrg_rf - ind_rama)/.N)/mean(ind_rama, na.rm = TRUE))*100,
                                      rmse = sqrt(sum((imrg_rf - ind_rama)^2)/.N), 
-                                     nrmse = sqrt(sum((imrg_rf - ind_rama)^2)/.N)/sd(ind_rama), 
+                                     #rrmse = (sqrt(sum((imrg_rf - ind_rama)^2)/.N)/mean(ind_rama, na.rm = TRUE))*100, 
+                                     rrmse = (sqrt(sum((imrg_rf - ind_rama)^2)/.N)/sqrt(sum(ind_rama)))*100, 
                                      mae = sum(abs(imrg_rf - ind_rama))/.N, 
+                                     rmae = ((sum(abs(imrg_rf - ind_rama))/.N) / mean(ind_rama, na.rm = TRUE))*100,
                                      cor = cor(imrg_rf, ind_rama), 
                                      ocn = factor('ind')), by = .(sname, imrg_run)]
 
