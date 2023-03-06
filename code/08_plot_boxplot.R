e73426f00fa0bc27184f5103198ffc9e95e24813
Rajani462 <53518303+Rajani462@users.noreply.github.com>
2023-03-06 21:12:46 +0100

    labelled sub panels

--- code/08_plot_boxplot.R
+++ code/08_plot_boxplot.R
@@ -2,6 +2,9 @@ source('./source/libs.R')
 source('./source/themes.R')
 source('./source/palettes.R')
 
+#devtools::install_github("eliocamp/tagger")
+library(tagger)
+
 
 # import the datsets ----------------------------------------------------------------
 
@@ -13,7 +16,7 @@ west_pacf <- readRDS("./data/metrics_west_pacf.rds")
 
 # pre-process datsets ----------------------------------------------------------------
 
-ind <- ind[, .(sname, imrg_run, bias, rmse, mae, POD, cor, FAR, CSI, ocn)]
+ind <- ind[, .(sname, imrg_run, bias, rmse, mae, cor, POD, FAR, CSI, ocn)]
 atln <- atln[, .(sname, imrg_run, bias, rmse, mae, cor, POD, FAR, CSI, ocn)]
 east_pacf <- east_pacf[, .(sname, imrg_run, bias, rmse, mae, cor, POD, FAR, CSI, ocn)]
 west_pacf <- west_pacf[, .(sname, imrg_run, bias, rmse, mae, cor, POD, FAR, CSI, ocn)]
@@ -33,7 +36,7 @@ levels(met_all_plot$variable) <- c("Bias (mm/day)", "RMSE (mm/day)", "MAE (mm/da
 levels(met_all_plot$imrg_run) <- c("IMERG-E", "IMERG-L", "IMERG-F")
 levels(met_all_plot$ocn) <- c("Indian", "Atlantic", "East Pacific", "West Pacific")
 
-ggplot(met_all_plot[variable != "COR"], aes(ocn, value, fill = imrg_run)) + 
+plot_metrics <- ggplot(met_all_plot[variable != "COR"], aes(ocn, value, fill = imrg_run)) + 
   geom_boxplot() + 
   facet_wrap(~variable, scales = "free_y", ncol = 3) + 
   scale_fill_manual(values = c("808000",  "#D35C37", "#6590bb")) + 
@@ -48,12 +51,29 @@ ggplot(met_all_plot[variable != "COR"], aes(ocn, value, fill = imrg_run)) +
         axis.text.x = element_text(angle = 30, hjust = 0.8, vjust = 0.9))
 #strip.text.x = element_text(size = 12))
 
-
 ggsave("results/paper_fig/boxplot_metrices.png",
        width = 7.2, height = 5.5, units = "in", dpi = 600)
 
 
-### with correlation
+## assign labels for sub-panels
+
+tag_facet2 <- function(p, open = "(", close = ")", tag_pool = letters, x = -Inf, y = Inf, 
+                       hjust = -0.5, vjust = 1.5, fontface = 2, family = font, face = "bold", ...) {
+  
+  gb <- ggplot_build(p)
+  lay <- gb$layout$layout
+  tags <- cbind(lay, label = paste0(open, tag_pool[lay$PANEL], close), x = x, y = y)
+  p + geom_text(data = tags, aes_string(x = "x", y = "y", label = "label"), ..., hjust = hjust, 
+                vjust = vjust, fontface = fontface, family = family, inherit.aes = FALSE)
+}
+
+
+tag_facet2(plot_metrics, open = NULL, hjust = -0.2, vjust = 0.9, fontface = 1, size = 3.5)
+ggsave("results/paper_fig/boxplot_metrices.png",
+       width = 7.2, height = 5.5, units = "in", dpi = 600)
+
+
+# with correlation -----------------------------------------------------
 
 ggplot(met_all_plot[variable != "MAE" ], aes(ocn, value, fill = imrg_run)) + 
   geom_boxplot() + 
@@ -74,7 +94,7 @@ ggsave("results/boxplot_metrices.png",
        width = 7.2, height = 5.5, units = "in", dpi = 600)
 
 
-### plot for poster presentation
+# plot for poster presentation -----------------------------------------
 
 ggplot(met_all_plot[variable == "BIAS" | variable == "RMSE"| variable == "CSI"], aes(ocn, value, fill = imrg_run)) + 
   geom_boxplot() + 
